ea5b64319717e830df538301a755742aff0c6ed2
Samantha Hiles <samih11@att.net>
2025-01-18 09:55:03 -0500

    Initial Commit

---
+++ To Do Hackwich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To Do Hackwich
+//
+//  Created by Samantha Hiles on 1/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To Do Hackwich/To_Do_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_HackwichApp.swift
+//  To Do Hackwich
+//
+//  Created by Samantha Hiles on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
