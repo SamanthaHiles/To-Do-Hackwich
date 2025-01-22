e5cfdaaf332eb162c59d1c092a726118c8f7f848
Samantha Hiles <samih11@att.net>
2025-01-22 10:01:16 -0600

    Created custom ToDoList class

---
+++ To Do Hackwich/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  To Do Hackwich
+//
+//  Created by Samantha Hiles on 1/22/25.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}
