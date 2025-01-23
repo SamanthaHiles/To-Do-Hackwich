c244e722def7b2b3572822fd805e1e329ebba62c
Samantha Hiles <samih11@att.net>
2025-01-23 09:31:08 -0600

    Created additonal features to AddItemsView

--- To Do Hackwich/AddItemView.swift
+++ To Do Hackwich/AddItemView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct AddItemView: View {
+    @ObservedObject var toDoList: ToDoList
+    @State private var showingAddItemView = false
+    @State private var priority = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let priorities = ["High", "Medium", "Low"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Picker("Priority", selection: $priority) {
+                    ForEach(Self.priorities, id: \.self) { priority in
+                        Text(priority)
+                    }
+                }
+                TextField("Description", text: $description)
+                DatePicker("DueDate", selection: $dueDate, displayedComponents: .date)
+            }
+            .sheet(isPresented: $showingAddItemView, content: {
+                AddItemView(toDoList: toDoList)
+            })
+            .navigationBarTitle("Add New To-Do Item", displayMode: .inline)
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: { showingAddItemView = true
+            },label: {
+                Image(systemName: "plus")
+
+            }))
+        }
     }
 }
 
 #Preview {
-    AddItemView()
+    AddItemView(toDoList: ToDoList())
 }
+  
